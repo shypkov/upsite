b4446df1a2d1c1290f47ea6faced735f26e5437b
shypkov <plsql_developer@mail.ru>
2022-06-17 22:21:48 +0300

    sitever smooth update

--- src/components/Siteversion.vue
+++ src/components/Siteversion.vue
@@ -1,10 +1,11 @@
 <!-- Here goes Legacy code) -->
 <template>
-  <div class="Siteversion">
-    <ul>
-      Cryptosapiens Staking
-      <br />
-      Version 1.1.2
-    </ul>
+  <div class="Siteversion-title">
+    <span> Cryptosapiens Staking </span>
   </div>
+  <div
+    class="q-badge flex inline items-center no-wrap q-badge--single-line bg-accent text-text-white staking-header__beta"
+  >
+    Ver 1.1.2</div
+  >
 </template>
